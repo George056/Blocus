be0c3463fe29f9b60d4b05a4bcafc4be985e457b
George056 <70665330+George056@users.noreply.github.com>
2020-12-04 14:26:56 -0600

    Line clears added

--- Assets/Scripts/TetrominoMovement.cs
+++ Assets/Scripts/TetrominoMovement.cs
@@ -11,6 +11,7 @@ public class TetrominoMovement : MonoBehaviour
 
     private float previousTime;
     private float fallTime = 0.8f;
+    private static Transform[,,] grid = new Transform[Xmax, Ymax, Zmax];
 
     // Start is called before the first frame update
     void Start()
@@ -49,9 +50,84 @@ public class TetrominoMovement : MonoBehaviour
         if (Time.time - previousTime > ((Input.GetKeyDown(KeyCode.S)) ? (fallTime/10) : fallTime))
         {
             transform.position += new Vector3(0, -1, 0);
-            if(!ValidMove())
+            if (!ValidMove())
+            {
                 transform.position -= new Vector3(0, -1, 0);
+                AddToGrid();
+                CheckForLines();
+                this.enabled = false;
+                FindObjectOfType<Spawner>().NewTetromino();
+            }
             previousTime = Time.time;
+        }
+    }
+
+    void CheckForLines()
+    {
+        for(int y = Ymax - 1; y > 0 ; y--)
+        {
+            if (HasPlane(y))
+            {
+                DeleteLine(y);
+                RowDown(y);
+            }
+        }
+    }
+
+    bool HasPlane(int y)
+    {
+        for(int z = 0; z < Zmax; z++)
+        {
+            if (!HasLine(y, z))
+                return false;
+        }
+        return true;
+    }
+
+    bool HasLine(int y, int z)
+    {
+        for(int x = 0; x < Xmax; x++)
+        {
+            if (grid[x, y, z] == null)
+                return false;
+        }
+        return true;
+    }
+
+    void DeleteLine(int y)
+    {
+        for(int x = 0; x < Xmax; x++)
+        {
+            Destroy(grid[x, y, 0].gameObject);
+            grid[x, y, 0] = null;
+        }
+    }
+
+    void RowDown(int y)
+    {
+        for(int i = y; i < Ymax; i++)
+        {
+            for(int x = 0; x < Xmax; x++)
+            {
+                if(grid[x, i, 0] != null)
+                {
+                    grid[x, i - 1, 0] = grid[x, i, 0];
+                    grid[x, i, 0] = null;
+                    grid[x, i - 1, 0].transform.position -= new Vector3(0, 1, 0);
+                }
+            }
+        }
+    }
+
+    void AddToGrid()
+    {
+        foreach(Transform child in transform)
+        {
+            int roundedX = Mathf.RoundToInt(child.transform.position.x);
+            int roundedY = Mathf.RoundToInt(child.transform.position.y);
+            int roundedZ = Mathf.RoundToInt(child.transform.position.z);
+
+            grid[roundedX, roundedY, roundedZ] = child;
         }
     }
 
@@ -63,10 +139,12 @@ public class TetrominoMovement : MonoBehaviour
             int roundedY = Mathf.RoundToInt(child.transform.position.y);
             int roundedZ = Mathf.RoundToInt(child.transform.position.z);
 
-            if(roundedX < 0 || roundedX >= Xmax || roundedY < 0 || roundedY >= Ymax || roundedZ < 0 || roundedZ >= Zmax)
+            if (roundedX < 0 || roundedX >= Xmax || roundedY < 0 || roundedY >= Ymax || roundedZ < 0 || roundedZ >= Zmax)
             {
                 return false;
             }
+            else if (grid[roundedX, roundedY, roundedZ] != null)
+                return false;
         }
         return true;
     }
