f318c8c04bbd35b5835ed0411bc9a8b89ddd2cde
Maverick1037 <70665291+Maverick1037@users.noreply.github.com>
2020-12-03 20:09:47 -0600

    Created "BLOCUS" menu with buttons

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static void LoadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public static void Quit()
+    {
+        Application.Quit();
+    }
+
+}
