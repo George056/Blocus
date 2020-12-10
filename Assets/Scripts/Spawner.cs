99db0a90a451ad4c1c7a34747429d81398f42fe6
Maverick1037 <70665291+Maverick1037@users.noreply.github.com>
2020-12-10 16:48:42 -0600

    Fixed Next. Dont Break

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,38 +8,61 @@ public class Spawner : MonoBehaviour
     public GameObject[] Tetrominos;
 
     [HideInInspector] public GameObject HoldPiece;
-    [HideInInspector] public GameObject[] NextTetrominosList = new GameObject [4];
+    [HideInInspector] public GameObject[] NextTetrominosList;
 
+    private Vector3 NextTetPos = new Vector3(18f, 15f, -6);
+    private bool started = false;
     // Start is called before the first frame update
     void Start()
     {
         LoadTetList();
         NewTetromino();
+        started = true;
     }
 
     public void NewTetromino()
     {
-        GameObject piece = NextTetrominosList[0];
+        GameObject piece;
+        if (started == false)
+        {
+            piece = Tetrominos[Random.Range(0, Tetrominos.Length)];
+        }
+        else
+        {
+            piece = NextTetrominosList[0];
+        }
         piece.transform.position = transform.position;
+        piece.GetComponent<TetrominoMovement>().enabled = true;
+        RotateList();
+        SpawnNextTet();
 
         if (piece.GetComponent<TetrominoMovement>().ValidMove())
         {
+            if (started == false)
+                GameManager.instance.ActivePiece = Instantiate(piece, transform.position, Quaternion.identity);
+            else
+                GameManager.instance.ActivePiece = piece;
+
             piece.GetComponent<TetrominoMovement>().SetPieceActive(true);
-            GameManager.instance.ActivePiece = Instantiate(piece, transform.position, Quaternion.identity);
         }
         else
         {
             TetrominoMovement.Loss();
         }
-        RotateList();
+    }
+
+    public void SpawnNextTet()
+    {
+        NextTetrominosList[0] = Instantiate(NextTetrominosList[0], NextTetPos, Quaternion.identity);
+        NextTetrominosList[0].GetComponent<TetrominoMovement>().enabled = false;
     }
 
     public void LoadTetList()
     {
+        NextTetrominosList = new GameObject[4];
         for (int i = 0; i < 4; i++)
         {
-            GameObject piece = Tetrominos[Random.Range(0, Tetrominos.Length)];
-            NextTetrominosList[i] = piece;
+            NextTetrominosList[i] = Tetrominos[Random.Range(0, Tetrominos.Length)];
         }
     }
 
